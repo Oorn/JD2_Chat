08944a9a044da981d6771ac24767d90aba9c7c38
JohnSmith <pversnp@gmail.com>
2022-10-24 22:26:07 +0300

    preparation for Security rework - we want to deliver relevant ChatUser with all memberships loaded directly to secured methods, to remove extra database access

--- common/src/main/java/com/andrey/db_entities/chat_friendship/ChatFriendshipRepository.java
+++ common/src/main/java/com/andrey/db_entities/chat_friendship/ChatFriendshipRepository.java
@@ -1,10 +1,14 @@
 package com.andrey.db_entities.chat_friendship;
 
 import com.andrey.db_entities.chat_channel_membership.ChatChannelMembership;
+import com.andrey.db_entities.chat_user.ChatUser;
+import java.util.Collections;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ChatFriendshipRepository extends CrudRepository<ChatFriendship, Long>
@@ -12,4 +16,87 @@ public interface ChatFriendshipRepository extends CrudRepository<ChatFriendship,
         , PagingAndSortingRepository<ChatFriendship, Long> {
 
     ChatFriendship findChatFriendshipById(Long id);
+    List<ChatFriendship> findChatFriendshipsByUserWithGreaterID(ChatUser user);
+    List<ChatFriendship> findChatFriendshipsByUserWithLesserID(ChatUser user);
+
+    boolean existsByUserWithGreaterIDAndUserWithLesserIDAndStatus(ChatUser user1, ChatUser user2, FriendshipStatus status);
+
+    List<ChatFriendship> findChatFriendshipsByUserWithGreaterIDAndUserWithLesserID(ChatUser user1, ChatUser user2);
+
+    default List<ChatFriendship> findChatFriendshipsByUser(ChatUser user) {
+        List<ChatFriendship> result = findChatFriendshipsByUserWithGreaterID(user);
+        result.addAll(findChatFriendshipsByUserWithLesserID(user));
+        return result;
+    }
+
+    default boolean checkFriendshipStatus(ChatUser user1, ChatUser user2) {
+
+        if (user2.getId() > user1.getId())
+        {
+            ChatUser t = user1;
+            user1 = user2;
+            user2 = t;
+        }
+        return existsByUserWithGreaterIDAndUserWithLesserIDAndStatus(user1, user2, FriendshipStatus.ACTIVE);
+    }
+
+    default List<ChatFriendship> findFriendship(ChatUser user1, ChatUser user2) {
+
+        if (user2.getId() > user1.getId())
+        {
+            ChatUser t = user1;
+            user1 = user2;
+            user2 = t;
+        }
+        return findChatFriendshipsByUserWithGreaterIDAndUserWithLesserID(user1, user2);
+    }
+
+    default ChatFriendship createFriendship(ChatUser user1, ChatUser user2) {
+        if (user2.getId() > user1.getId())
+        {
+            ChatUser t = user1;
+            user1 = user2;
+            user2 = t;
+        }
+        List<ChatFriendship> oldFriendships = findChatFriendshipsByUserWithGreaterIDAndUserWithLesserID(user1, user2);
+        if (oldFriendships.isEmpty())
+        {
+            //create new
+            return saveAndFlush(ChatFriendship.builder()
+                    .userWithGreaterID(user1)
+                    .userWithLesserID(user2)
+                    .status(FriendshipStatus.ACTIVE)
+                    .build());
+        }
+        ChatFriendship oldFriendship = oldFriendships.get(0);
+        if (oldFriendship.getStatus() == FriendshipStatus.REMOVED) {
+            //change existing
+            oldFriendship.setStatus(FriendshipStatus.ACTIVE);
+            return saveAndFlush(oldFriendship);
+        }
+        //nothing needs changing
+        return oldFriendship;
+    }
+    default ChatFriendship removeFriendshipIfExists(ChatUser user1, ChatUser user2) {
+        if (user2.getId() > user1.getId())
+        {
+            ChatUser t = user1;
+            user1 = user2;
+            user2 = t;
+        }
+        List<ChatFriendship> oldFriendships = findChatFriendshipsByUserWithGreaterIDAndUserWithLesserID(user1, user2);
+        if (oldFriendships.isEmpty())
+        {
+            //nothing to change
+            return null;
+        }
+        ChatFriendship oldFriendship = oldFriendships.get(0);
+        if (oldFriendship.getStatus() == FriendshipStatus.REMOVED) {
+            //nothing to change
+            return oldFriendship;
+        }
+        //ser status to REMOVED
+        oldFriendship.setStatus(FriendshipStatus.REMOVED);
+        return saveAndFlush(oldFriendship);
+    }
 }
--- common/src/main/java/com/andrey/db_entities/chat_user/ChatUserRepository.java
+++ common/src/main/java/com/andrey/db_entities/chat_user/ChatUserRepository.java
@@ -1,6 +1,7 @@
 package com.andrey.db_entities.chat_user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -13,4 +14,13 @@ public interface ChatUserRepository extends CrudRepository<ChatUser, Long>
     ChatUser findChatUserById(Long id);
     ChatUser findChatUserByEmail(String email);
     Boolean existsByEmail(String email);
+
+    @Query(value = "select u from ChatUser u" +
+            " inner join fetch u.channelMemberships m" +
+            " inner join fetch m.channel mc" +
+            " inner join fetch u.friendshipsWithGreaterID f1" +
+            " inner join fetch f1.userWithLesserID f1u" +
+            " inner join fetch u.friendshipsWithLesserID f2" +
+            " inner join fetch f2.userWithGreaterID f2u")
+    ChatUser findChatUserByEmailWithFriendshipsAndChatMemberships (String email);
 }
