22206299684020fc8d13336f296e075ce2955db9
JohnSmith <pversnp@gmail.com>
2022-11-07 23:29:05 +0300

    fixed user blocks not working in profile channels

--- common/src/main/java/com/andrey/service/channel/ChannelNamingService.java
+++ common/src/main/java/com/andrey/service/channel/ChannelNamingService.java
@@ -3,7 +3,7 @@ package com.andrey.service.channel;
 public interface ChannelNamingService {
     String generatePrivateChannelName(long profileId1, long profileId2);
 
-    String generateProfileChannelName(long profileId1, long profileId2);
+    String generateProfileChannelName(long userId1, long userId2, long profileId1, long profileId2);
 
     long[] getMemberIdsFromChannelName(String channelName);
 }
